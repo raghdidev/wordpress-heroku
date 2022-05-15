d2b45bb48a6d33c5cbda8c1090b3198e4adc1fe3
raghdidev <105546907+raghdidev@users.noreply.github.com>
2022-05-15 22:37:51 +0100

    Update development.php

--- config/environments/development.php
+++ config/environments/development.php
@@ -3,6 +3,6 @@
 define('SAVEQUERIES', true);
 define('WP_DEBUG', true);
 define('SCRIPT_DEBUG', true);
-
+define('AUTOMATIC_UPDATER_DISABLED', true);
 /** Plugins */
 define('S3_UPLOADS_USE_LOCAL', true);
