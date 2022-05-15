3cbd6b25c632d0e8eb356b8dfcd7eb2a07b5990c
raghdidev <105546907+raghdidev@users.noreply.github.com>
2022-05-15 22:38:50 +0100

    Update production.php

--- config/environments/production.php
+++ config/environments/production.php
@@ -4,4 +4,5 @@ ini_set('display_errors', 0);
 define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
 /** Disable all file modifications including updates and update notifications */
+define('AUTOMATIC_UPDATER_DISABLED', true);
 define('DISALLOW_FILE_MODS', false);
