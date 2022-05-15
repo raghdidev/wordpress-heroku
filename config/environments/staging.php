9a1b6af753006aa152e895c2d21e6ed17c0aed14
raghdidev <105546907+raghdidev@users.noreply.github.com>
2022-05-15 22:39:22 +0100

    Update staging.php

--- config/environments/staging.php
+++ config/environments/staging.php
@@ -4,6 +4,7 @@ ini_set('display_errors', 0);
 define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
 /** Disable all file modifications including updates and update notifications */
+define('AUTOMATIC_UPDATER_DISABLED', true);
 define('DISALLOW_FILE_MODS', false);
 
 /** Plugins */
